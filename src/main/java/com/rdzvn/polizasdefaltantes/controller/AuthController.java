d5e125274480d94ff2d4e2a58ff4be7005d35c11
Aldordzvn <aldordzvn2001@gmail.com>
2026-06-01 14:55:17 -0700

    ultimo paso security

---
+++ src/main/java/com/rdzvn/polizasdefaltantes/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.rdzvn.polizasdefaltantes.controller;
+
+import com.rdzvn.polizasdefaltantes.dto.request.AuthRequest;
+import com.rdzvn.polizasdefaltantes.security.JwtUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login (@RequestBody AuthRequest request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        String token = jwtUtil.generateToken(request.getUsername());
+        return ResponseEntity.ok(Map.of("token", token));
+    }
+}
