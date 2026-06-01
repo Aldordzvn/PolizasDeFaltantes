f974675056b4735d7e4249a7e4672465d4c96832
Aldordzvn <aldordzvn2001@gmail.com>
2026-06-01 14:08:28 -0700

    primer commit

---
+++ src/main/java/com/rdzvn/polizasdefaltantes/PolizasdefaltantesApplication.java
@@ -0,0 +1,13 @@
+package com.rdzvn.polizasdefaltantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolizasdefaltantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolizasdefaltantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rdzvn/polizasdefaltantes/PolizasdefaltantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdzvn.polizasdefaltantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolizasdefaltantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
